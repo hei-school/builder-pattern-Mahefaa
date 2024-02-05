bb841286dc5810239cf1ef167ed8dbed2fd67053
Mahefaa <hei.mahefa@gmail.com>
2024-02-05 12:16:12 +0300

    chore: simulate car making from scratch and via director in Main class

--- src/main/java/org/schoolproject/Main.java
+++ src/main/java/org/schoolproject/Main.java
@@ -1,7 +1,22 @@
 package org.schoolproject;
 
+import org.schoolproject.car.CarBuilder;
+import org.schoolproject.manual.CarManualBuilder;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world");
+    Director director = new Director();
+    CarBuilder carBuilder = new CarBuilder();
+    CarManualBuilder carManualBuilder = new CarManualBuilder();
+
+    director.makeSUV(carBuilder);
+    director.makeSUV(carManualBuilder);
+    System.out.println(carBuilder.getResult());
+    System.out.println(carManualBuilder.getResult());
+
+    director.makeSportsCar(carBuilder);
+    director.makeSportsCar(carManualBuilder);
+    System.out.println(carBuilder.getResult());
+    System.out.println(carManualBuilder.getResult());
   }
 }
